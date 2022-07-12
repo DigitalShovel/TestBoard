3f8a3334a1064b5d6b2a5f8200184e69b04d6d73
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-07-12 15:14:48 -0400

    removed alerts

--- app/js/script.js
+++ app/js/script.js
@@ -2,7 +2,7 @@ function WebSocketTest() {
    let inputData = document.getElementById("inputData").value;
 
    if ("WebSocket" in window) {
-      alert("WebSocket is supported by your Browser!");
+      // alert("WebSocket is supported by your Browser!");
 
       // Let us open a web socket
       var ws = new WebSocket("wss://42jjhgjbdc.execute-api.us-east-1.amazonaws.com/production");
@@ -11,12 +11,12 @@ function WebSocketTest() {
          // Web Socket is connected, send data using send()
          ws.send("{\"action\": \"sendMessage\",\"message\":" + inputData + "}");
          console.log(inputData)
-         alert("Message is sent...");
+         // alert("Message is sent...");
       };
 
       ws.onmessage = function (evt) {
          var received_msg = evt.data;
-         alert("Message is received...");
+         // alert("Message is received...");
       };
 
       ws.onclose = function () {
