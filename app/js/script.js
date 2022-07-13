043193cb9d89fd3455adb2a4f87ff3aba99dc4e6
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-07-12 21:35:01 -0400

    Added

--- app/js/script.js
+++ app/js/script.js
@@ -97,7 +97,8 @@ function readItem() {
   };
 
   var item = {
-    TableName: "IoT_Result_ESP32"
+    TableName: "IoT_Result_ESP32",
+    KeyConditionExpression: 'MacAddress = :MacAddress'
   };
   docClient.query(item, onScan);
 
