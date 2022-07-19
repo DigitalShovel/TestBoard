05b2c32b14b1077a74ebca0f43ae294d601bf28b
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-07-19 11:18:02 -0400

    Changed function readCT

--- app/js/script.js
+++ app/js/script.js
@@ -85,7 +85,7 @@ function auth() {
 /////////////////////////////////////////////////////////
 
 ///////////// Read CT Values from DB ////////////////////
-function readCT(channel, ctnum) {
+function readCT() {
   var docClient = new AWS.DynamoDB.DocumentClient();
   var ctItem = {
     TableName: "IoT_Result",
