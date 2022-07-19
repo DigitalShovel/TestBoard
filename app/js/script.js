48461efb4c24fda370e1ea73b572f843e38ed76c
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-07-19 14:32:30 -0400

    Printing to console the test result

--- app/js/script.js
+++ app/js/script.js
@@ -100,7 +100,7 @@ function readCT() {
       alert(JSON.stringify(err, undefined, 2));
     }
     else {
-      console.log(JSON.stringify(data, undefined, 2))
+      console.log(JSON.stringify(data['Items']['Time'], undefined, 2))
     }
   });
 }
