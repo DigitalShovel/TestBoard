caf79a074634eae2f3dfa6c85dc54da7bfd47fd2
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-07-20 08:41:51 -0400

    Empty Arrays

--- app/js/chartConfig.js
+++ app/js/chartConfig.js
@@ -1,11 +1,11 @@
 //////////// Setup ////////////////
 const data = {
-  labels: ['January', 'Febuary'],
+  labels: [],
   autoPadding: true,
   datasets: [{
     yAxisID: 'Voltage',
     label: 'CT Measurements',
-    data: [65, -10],
+    data: [],
     fill: false,
     borderColor: 'rgb(75, 192, 192)',
     tension: 0.1
