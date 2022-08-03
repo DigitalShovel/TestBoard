b3403df1b1b1abe68bcbd604eff900e4d0ca5770
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-08-03 13:06:02 -0400

    C2

--- app/js/popup.js
+++ app/js/popup.js
@@ -8,8 +8,8 @@ let listOfViewGraph = [];
 let listOfViewGraphID = [['G1-1', 'C1-1'], ['G1-2', 'C1-2']];
 
 for (var i=0; i<2; i++){
-  listOfViewGraph.push(document.getElementById(listOfViewGraphID[i]));
-  listOfViewGraphID[i][0].addEventListener("click", () => {
+  listOfViewGraph.push(document.getElementById(listOfViewGraphID[i][0]));
+  listOfViewGraph[i][0].addEventListener("click", () => {
     popup.classList.toggle("popup__show");
   });
 }
