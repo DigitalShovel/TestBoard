8dfd15d6282f0a2f971a0f282f702e7ef8600be2
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-08-03 13:14:16 -0400

    C3

--- app/js/popup.js
+++ app/js/popup.js
@@ -2,14 +2,17 @@
 //let popupBtnHide = document.querySelector(".popup__btn-hide");
 
 let popup = document.querySelector(".popup");
-//let popupHide = document.querySelector(".popup__hide");
+let popupHide = document.querySelector(".popup__hide");
 
 let listOfViewGraph = [];
 let listOfViewGraphID = [['G1-1', 'C1-1'], ['G1-2', 'C1-2']];
 
 for (var i=0; i<2; i++){
-  listOfViewGraph.push(document.getElementById(listOfViewGraphID[i][0]));
-  listOfViewGraph[i].addEventListener("click", () => {
+  listOfViewGraph.pushArray(document.getElementById(listOfViewGraphID[i][0]), document.getElementById(listOfViewGraphID[i][1]));
+  listOfViewGraph[i][0].addEventListener("click", () => {
+    popup.classList.toggle("popup__show");
+  });
+  listOfViewGraph[i][1].addEventListener("click", () => {
     popup.classList.toggle("popup__show");
   });
 }
