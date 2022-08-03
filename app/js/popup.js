b4742d94ef326af297ce9b3c3eadcd4307a44272
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-08-03 12:59:35 -0400

    Checking View Graph #1

--- app/js/popup.js
+++ app/js/popup.js
@@ -2,23 +2,21 @@
 //let popupBtnHide = document.querySelector(".popup__btn-hide");
 
 let listOfViewGraph = [];
-let listOfViewGraphID = ['G1-1', 'G1-2'];
+let listOfViewGraphID = [['G1-1', 'C1-1'], ['G1-2', 'C1-2']];
 
 for (var i=0; i<2; i++){
   listOfViewGraph.push(document.getElementById(listOfViewGraphID[i]));
 }
 
 let popup = document.querySelector(".popup");
-let popupHide = document.querySelector(".popup__hide");
+//let popupHide = document.querySelector(".popup__hide");
 
-listOfViewGraph[0].addEventListener("click", () => {
-  popup.classList.toggle("popup__show");
-});
+for (var i=0; i<2; i++){
+  listOfViewGraphID[i][0].addEventListener("click", () => {
+    popup.classList.toggle("popup__show");
+  });
 
-listOfViewGraph[1].addEventListener("click", () => {
-  popup.classList.toggle("popup__show");
-});
-
-popupBtnHide.addEventListener("click", () => {
-  popup.classList.toggle("popup__show");
-});
+  listOfViewGraphID[i][1].addEventListener("click", () => {
+    popup.classList.toggle("popup__show");
+  });
+}
