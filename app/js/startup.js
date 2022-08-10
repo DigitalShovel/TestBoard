713ce6148def81de058d314461d9fa48b86abbb2
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-08-10 12:17:36 -0400

    Deleted startup

--- app/js/startup.js
+++
@@ -1,9 +0,0 @@
-/////// Functions to run if login is Authorized ////////
-import { readItem, readCT } from "./script"
-
-readItem();
-/////////////// Refresh chart every 5 seconds /////////////
-var inverval_timer = setInterval(function () {
-    readCT();
-}, 5000);
-/////////////////////////////////////////////////////////
