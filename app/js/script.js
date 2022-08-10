139ca3db5e5c51e3d9ed5990d049dfd81c1416cd
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-08-10 10:50:15 -0400

    Test Startup

--- app/js/script.js
+++ app/js/script.js
@@ -91,18 +91,7 @@ function auth() {
             "cognito-idp.ca-central-1.amazonaws.com/ca-central-1_Lf5pWzdj2": idToken
           }
         }); 
-  loadOnLogin();      /// Load all the function needed, including creating objects
-}
-/////////////////////////////////////////////////////////
-
-/////// Functions to run if login is Authorized ////////
-
-function loadOnLogin() {
-  readItem();
-  /////////////// Refresh chart every 5 seconds /////////////
-  var inverval_timer = setInterval(function () {
-    readCT();
-  }, 5000);
+  //loadOnLogin();      /// Load all the function needed, including creating objects
 }
 /////////////////////////////////////////////////////////
 
---
+++ app/js/startup.js
@@ -0,0 +1,7 @@
+/////// Functions to run if login is Authorized ////////
+readItem();
+/////////////// Refresh chart every 5 seconds /////////////
+var inverval_timer = setInterval(function () {
+    readCT();
+}, 5000);
+/////////////////////////////////////////////////////////
