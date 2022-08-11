edb416fa0727c889d35aab588078777006bcad6d
HugoRodrigues87 <80361481+HugoRodrigues87@users.noreply.github.com>
2022-08-11 15:58:54 -0400

    Add change of color on progress circle

--- app/js/progress-circle.js
+++ app/js/progress-circle.js
@@ -9,7 +9,12 @@ function updateProgressCircle(id){
 function setProgress(id, textid, testNumber, testTotal) {
   var percentage = Math.round((testNumber/testTotal)*100);
   const offset = circumference - (testNumber/testTotal) * circumference;
+  if (percentage > 0 && percentage < 100){
+    document.getElementById(id).style.stroke = "var(--cool-blue-600)";
+  }
+  else if (percentage == 100){
+    document.getElementById(id).style.stroke = "var(--success-600)";
+  }
   document.getElementById(id).style.strokeDashoffset = offset;
   document.getElementById(textid).innerHTML = String(percentage)+"%";
-  console.log("TEXT: ", document.getElementById(textid).innerText);
 }
