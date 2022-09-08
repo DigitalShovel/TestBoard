6d9d8951d02e92cc4b1ef0514d49442a4c46e314
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-09-08 11:34:50 -0400

    last padding bottom change to scrollbar

--- app/js/chartConfig.js
+++ app/js/chartConfig.js
@@ -107,7 +107,7 @@ var moveChart = {
     let drawCircleRight = new CircleChevron();
     drawCircleRight.draw(ctx, right, -5);*/
     /////////// Draw a scroll bar /////////////
-    const bpix = chart.options.layout.padding.bottom + 32;
+    const bpix = chart.options.layout.padding.bottom + 48;
     ctx.beginPath();
     ctx.fillStyle = scrollbarColor;
     ctx.rect(left + 15, bottom + bpix, width - 30, 8);
